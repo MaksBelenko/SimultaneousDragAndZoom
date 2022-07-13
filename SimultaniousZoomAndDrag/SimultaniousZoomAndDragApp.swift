73a7441ba261400db18e2e77e6c3417ddd8e7bd2
MaksBelenko <maksim.belenko@gmail.com>
2022-07-13 21:19:52 +0100

    Initial Commit

---
+++ SimultaniousZoomAndDrag/SimultaniousZoomAndDragApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimultaniousZoomAndDragApp.swift
+//  SimultaniousZoomAndDrag
+//
+//  Created by Maksim on 13/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SimultaniousZoomAndDragApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
