014484f9982f3c086230789c8b3ceb4dae048a85
MaksBelenko <maksim.belenko@gmail.com>
2022-07-13 22:29:31 +0100

    Pinch and drag implementation

--- SimultaniousZoomAndDrag/ContentView.swift
+++ SimultaniousZoomAndDrag/ContentView.swift
@@ -9,11 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ScrollView(.vertical, showsIndicators: false) {
+            LazyVStack {
+                ForEach(1...5, id: \.self) { index in
+                    Image("image")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: getRect().width - 30, height: 250)
+                        .cornerRadius(15)
+                        .addPinchToZoom()
+                }
+                
+            }
+            .padding()
         }
     }
 }
@@ -22,4 +31,136 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+extension View {
+    
+    func addPinchToZoom() -> some View {
+        return PinchZoomContext { self }
+    }
+    
+    func getRect() -> CGRect {
+        return UIScreen.main.bounds
+    }
+    
+}
+
+struct PinchZoomContext<Content: View>: View {
+    
+    var content: Content
+    
+    init(@ViewBuilder content: @escaping () -> Content) {
+        self.content = content()
+    }
+    
+    @State private var offset = CGPoint.zero
+    @State private var scale = CGFloat.zero
+    @State private var scalePosition = CGPoint.zero
+    
+    
+    var body: some View {
+        content
+            .offset(x: offset.x, y: offset.y)
+            .overlay {
+                GeometryReader { proxy in
+                    let size = proxy.size
+
+                    ZoomGesture(size: size, scale: $scale, offset: $offset, scalePosition: $scalePosition)
+                }
+            }
+            .scaleEffect(1 + (scale < 0 ? 0 : scale), anchor: .init(x: scalePosition.x, y: scalePosition.y))
+            .zIndex(scale != 0 ? 1000 : 0)
+            .onChange(of: scale) { newValue in
+                if scale == -1 {
+                    // Give some time to finish animation
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+                        scale = 0
+                    }
+                }
+            }
+    }
+    
+}
+
+struct ZoomGesture: UIViewRepresentable {
+    
+    var size: CGSize
+    
+    @Binding var scale: CGFloat
+    @Binding var offset: CGPoint
+    
+    @Binding var scalePosition: CGPoint
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(parent: self)
+    }
+    
+    func makeUIView(context: Context) -> some UIView {
+        let view = UIView()
+        view.backgroundColor = .clear
+        
+        let pinchGesture = UIPinchGestureRecognizer(target: context.coordinator, action: #selector(context.coordinator.handlePinch(sender:)))
+        
+        let panGesture = UIPanGestureRecognizer(target: context.coordinator, action: #selector(context.coordinator.handlePan(sender:)))
+        
+        panGesture.delegate = context.coordinator
+        
+        view.addGestureRecognizer(pinchGesture)
+        view.addGestureRecognizer(panGesture)
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
+    class Coordinator: NSObject, UIGestureRecognizerDelegate {
+        
+        var parent: ZoomGesture
+        
+        init(parent: ZoomGesture) {
+            self.parent = parent
+        }
+        
+        func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+            return true
+        }
+        
+        @objc func handlePan(sender: UIPanGestureRecognizer) {
+            sender.minimumNumberOfTouches = 2
+            
+            if (sender.state == .began || sender.state == .changed) && parent.scale > 0 {
+                if let view = sender.view {
+                    let translation = sender.translation(in: view)
+                    parent.offset = translation
+                }
+            } else {
+                withAnimation {
+                    parent.offset = .zero
+                }
+            }
+        }
+        
+        @objc func handlePinch(sender: UIPinchGestureRecognizer) {
+            if sender.state == .began || sender.state == .changed {
+                parent.scale = sender.scale - 1
+                
+                let scalePoint  = CGPoint(
+                    x: sender.location(in: sender.view).x / sender.view!.frame.size.width,
+                    y: sender.location(in: sender.view).y / sender.view!.frame.size.height
+                )
+                
+                // result will be ((0...1),(0...1))
+                parent.scalePosition = parent.scalePosition == .zero ? scalePoint : parent.scalePosition
+                
+            } else {
+                withAnimation(.easeInOut(duration: 0.35)) {
+                    parent.scale = -1
+                    parent.scalePosition = .zero
+                }
+            }
+        }
+    }
+    
 }
